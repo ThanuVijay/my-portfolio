8263bbcc311f4954b827e7c19e760ee9dc54cf68
DESKTOP-VS26VVU\vijay <vijayaratnamythanushan000@gmail.com>
2024-02-04 08:05:54 +0530

    img-crrct

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/Thanu.png";
+import myImg from "../../Assets/Thanu.jpeg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
